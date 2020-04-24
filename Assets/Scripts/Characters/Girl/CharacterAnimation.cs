ed3fbb8ec1081adafaea2d072d0adf47ef0e54fc
Erik Takoev <erik.takoev@4friendsplayrix.com>
2020-04-25 02:31:42 +0300

    Add girl and platforms

---
+++ Assets/Scripts/Characters/Girl/CharacterAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    public Sprite[] IdleSprites;
+    int indexSprite;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        spriteRenderer.sprite = IdleSprites[indexSprite];
+
+
+        indexSprite++;
+        indexSprite = indexSprite % IdleSprites.Length;
+
+    }
+}
---
+++ Assets/Scripts/Characters/Girl/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    CharacterAnimation characterAnimation;
+    Rigidbody2D rigidbody2D;
+
+    float velocityY = 0;
+
+    private void Awake()
+    {
+        characterAnimation = GetComponent<CharacterAnimation>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            velocityY = 5;
+        }
+        rigidbody2D.velocity = new Vector2(4f, velocityY);
+
+        if(velocityY > -5f)
+        {
+            velocityY -= 0.1f;
+            if (velocityY < -5f)
+                velocityY = -5f;
+        }
+
+    }
+}
