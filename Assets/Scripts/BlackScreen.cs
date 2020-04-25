f4d36f54625a219fadbed458c79db56c0ee57227
Erik Takoev <erik.takoev@4friendsplayrix.com>
2020-04-25 22:16:42 +0300

    End game

---
+++ Assets/Scripts/BlackScreen.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackScreen : MonoBehaviour
+{
+    public Image BlackBar;
+
+    bool startGame = true;
+    float alpha;
+
+    private void Awake()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        BlackBar.color = Color.black;
+        alpha = 1f;
+    }
+
+
+    public void EndGame()
+    {
+        startGame = false;
+    }
+
+    private void Update()
+    {
+        if(startGame && alpha != 0)
+        {
+            alpha -= Time.deltaTime * 2f;
+            if(alpha < 0)
+            {
+                alpha = 0;
+            }
+            BlackBar.color = new Color(1, 1, 1, alpha);
+        }
+        else if(!startGame && alpha != 1)
+        {
+            alpha += Time.deltaTime * 2f;
+            if (alpha > 1)
+            {
+                alpha = 1;
+
+                GameLoader.Reload();
+            }
+            BlackBar.color = new Color(1, 1, 1, alpha);
+        }
+    }
+}
--- Assets/Scripts/World/ObjectGenerator.cs
+++ Assets/Scripts/World/ObjectGenerator.cs
@@ -3,9 +3,12 @@
 public class ObjectGenerator : MonoBehaviour
 {
     public float TimeGen;
-    public Vector3 Velocity;
+    public Vector3 MinVelocity;
+    public Vector3 MaxVelocity;
     public GameObject ObjectPrefab;
-    public Vector3 ObjectScale = Vector3.one;
+    public float ObjectMinScale = 0.5f;
+    public float ObjectMaxScale = 1f;
+    public float DestroyTime = 5;
 
 
     float lefttime;
@@ -20,12 +23,15 @@ public class ObjectGenerator : MonoBehaviour
             lefttime = 0;
 
             GameObject go = Instantiate(ObjectPrefab, transform);
-            go.transform.localScale = ObjectScale;
+            go.transform.localScale = Random.Range(ObjectMinScale, ObjectMaxScale) * Vector3.one;
             go.transform.position = transform.position;
 
             Rigidbody2D r = go.GetComponent<Rigidbody2D>();
-            r.AddForce(Velocity, ForceMode2D.Impulse);
+            r.AddForce(new Vector3(Random.Range(MinVelocity.x, MaxVelocity.x), Random.Range(MinVelocity.y, MinVelocity.y)), ForceMode2D.Impulse);
             r.AddTorque(100);
+
+            if (DestroyTime != 0f)
+                Destroy(go, DestroyTime);
         }
     }
 }
