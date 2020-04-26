2d5e006f9ad4f5f560356f6c183bbd2879e75147
Erik Takoev <erik.takoev@4friendsplayrix.com>
2020-04-26 14:15:21 +0300

    new world

---
+++ Assets/Scripts/EnableGravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableGravity : MonoBehaviour
+{
+    Rigidbody2D Rigidbody2D;
+    public float GravityScale = 0.5f;
+
+    private void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+
+        Rigidbody2D.gravityScale = 0;
+
+    }
+
+    public void Gravity()
+    {
+        Rigidbody2D.gravityScale = GravityScale;
+    }
+}
