a58717982d24a6bad87388cf01ffab66ea4648e4
Андрей Воскобойник <andrey.voskoboinyk@4friendsplayrix.com>
2020-04-26 03:34:22 +0300

    Add Intro.mp4 (stub) and connect it to the logic

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,22 +1,90 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private VideoPlayer player;
+    [SerializeField] private RawImage rawImage;
+    [SerializeField] private Image firstFrame;
     [SerializeField] private Button button;
     [SerializeField] private string sceneName;
+
+    private static bool introShown = false;
     
     void Start()
     {
+        if (introShown)
+        {
+            ShowButton();
+            return;
+        }
+
+        if (button != null)
+        {
+            button.gameObject.SetActive(false);
+        }
+
+        if (player == null || rawImage == null)
+        {
+            Debug.Assert(false);
+            ShowButton();
+            return;
+        }
+
+        player.prepareCompleted += OnPrepared;
+        player.errorReceived += OnVideoError;
+        player.loopPointReached += OnVideoEnd;
+        player.Prepare();
+    }
+
+    private void OnPrepared(VideoPlayer player)
+    {
+        if (firstFrame != null)
+        {
+            firstFrame.gameObject.SetActive(false);
+        }
+
+        player.Play();
+        rawImage.texture = player.texture;
+    }
+
+    private void OnVideoError(VideoPlayer player, string error)
+    {
+        Debug.LogError("Cannot play Intro due to error '" + error + "'!");
+        ShowButton();
+    }
+
+    private void OnVideoEnd(VideoPlayer player)
+    {
+        Debug.Log("Intro finished.");
+        introShown = true;
+        ShowButton();
+    }
+
+    private void ShowButton()
+    {
+        if (firstFrame != null)
+        {
+            firstFrame.gameObject.SetActive(false);
+        }
+
+        if (player != null)
+        {
+            player.prepareCompleted -= OnPrepared;
+            player.errorReceived -= OnVideoError;
+            player.loopPointReached -= OnVideoEnd;
+            player.gameObject.SetActive(false);
+        }
+
         if (button == null)
         {
             LoadScene();
         }
         else
         {
+            button.gameObject.SetActive(true);
             button.onClick.AddListener(LoadScene);
         }
     }
