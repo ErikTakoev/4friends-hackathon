45a94f63a8d27e6040d89d3b984169e7f4d62380
Андрей Воскобойник <andrey.voskoboinyk@4friendsplayrix.com>
2020-04-26 10:48:42 +0300

    Use proper formula for parallax

--- Assets/Scripts/ParallaxController.cs
+++ Assets/Scripts/ParallaxController.cs
@@ -1,56 +1,36 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [System.Serializable]
 public class ParallaxGameObject
 {
 	public Transform transform;
-	public Vector3 coef;
+	public Vector3 coef = Vector3.one;
 }
 
 public class ParallaxController : MonoBehaviour
 {
+	[SerializeField] private PlayerController playerController;
     [SerializeField] private ParallaxGameObject[] transformParameters;
-	[SerializeField] private float accelCoef = 0.05f;
 
-	Dictionary<Transform, Vector3> initialPositions;
-	Vector3 screenCenter;
-	float additionalCoef = 1000;
+	Vector3 prevPos;
 
     private void Awake()
     {
-		screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, 0);
-		initialPositions = new Dictionary<Transform, Vector3>();
-
-		foreach (ParallaxGameObject data in transformParameters)
-		{
-			if (data.transform == null)
-				continue;
-
-			initialPositions.Add(data.transform, data.transform.localPosition);
-		}
+		playerController.SubscribeForMove(OnMove);
 	}
 
-    private void Update()
-    {
-		if (transformParameters.Length == 0 || accelCoef == 0.0f)
-		{
-			return;
-		}
-
-		Vector3 lastPos = Input.mousePosition;
-		Vector3 shift = (lastPos - screenCenter) * additionalCoef;
-
+	private void OnMove(Vector3 newPos)
+	{
+		Vector3 shift = newPos - prevPos;
 		foreach (ParallaxGameObject data in transformParameters)
 		{
 			if (data.transform == null || data.coef == Vector3.zero)
 				continue;
 
-			Vector3 initialPos = initialPositions[data.transform];
-			float newX = initialPos.x + Mathf.Sign(shift.x) * Mathf.Sqrt(Mathf.Abs(shift.x)) * data.coef.x * accelCoef / additionalCoef;
-			float newY = initialPos.y + Mathf.Sign(shift.y) * Mathf.Sqrt(Mathf.Abs(shift.y)) * data.coef.y * accelCoef / additionalCoef;
-
-			data.transform.localPosition = new Vector3(newX, newY, 0);
+			Vector3 objShift = shift;
+			objShift.Scale(data.coef);
+			data.transform.localPosition += objShift;
 		}
+		prevPos = newPos;
 	}
 }
